8ea79c594a3f842a9982b6eb875b82ddcbaeb8aa
Matsuu <96949190+matsudev-bit@users.noreply.github.com>
2022-04-17 00:59:28 +0300

    Add files via upload

---
+++ mars html playground (1)/maincode.js
@@ -0,0 +1,56 @@
+function setupEditor(){
+    window.editor = ace.edit("editor");
+    editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/html");
+    editor.setValue(`<!DOCTYPE html>
+  <html>
+  <head>
+  
+  </head>
+  
+  <body>
+  
+  </body>
+  
+  </html>`,1); //1 = moves cursor to end
+  
+    
+  
+    editor.focus();
+    
+    
+    editor.setOptions({
+      fontSize: "16pt",
+      showLineNumbers: false,
+      showGutter: false,
+      vScrollBarAlwaysVisible:true,
+      enableBasicAutocompletion: false, enableLiveAutocompletion: false
+    });
+  
+    editor.setShowPrintMargin(false);
+    editor.setBehavioursEnabled(false);
+  }
+  
+  function update(){
+    var idoc = document.getElementById("iframe").contentWindow.document;
+    idoc.open();
+    idoc.write(editor.getValue());
+    idoc.close();
+  
+  
+  }
+  function lightmode(){
+    
+    editor.setTheme("ace/theme/chrome");
+  
+  
+  }
+  function darkmode(){
+    editor.setTheme("ace/theme/monokai");
+  
+  }
+  
+  function ready(){
+    setupEditor();
+  
+  }
